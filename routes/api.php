6c630ff4490ae3d1146f2246597e489f1756141a
simoatm234 <atmsimo599@gmail.com>
2026-04-29 14:47:04 +0100

    improve routes structer

--- routes/api.php
+++ routes/api.php
@@ -5,27 +5,39 @@ use App\Http\Controllers\emailConfirmation;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtAuthMiddleware;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
 
-
-Route::prefix('/')->group(function (){
-//  user routes
-//  without auth 
-Route::prefix('users')->group(function ()  {
-    Route::get('/show/All' , [UserController::class , 'index'])->middleware([JwtAuthMiddleware::class]);
-    Route::post('/store' , [UserController::class , 'store']);
-    Route::post('/email-confirm/{email}' , [emailConfirmation::class , 'confirmingEmail']);
-    Route::post('/verifyResetToken/{email}' , [emailConfirmation::class , 'verifyResetToken']);
-    Route::post('/updatePassword' , [UserController::class , 'updatePassword']);
-    Route::put('/update/{user}' , [UserController::class , 'update'])->middleware([JwtAuthMiddleware::class]);
-    Route::delete('/destroy/{user}' , [UserController::class , 'destroy'])->middleware([JwtAuthMiddleware::class]);
-});
-// auth routes
+// PUBLIC ROUTES
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout' , [AuthController::class , 'logout'])->middleware([JwtAuthMiddleware::class]);
-Route::post('send-acount-confirmation' , [AuthController::class , 'manualSendEmailValidation']);
-Route::post('send-reset-password-token' , [AuthController::class , 'sendResetPassToken']);
+Route::post('register', [UserController::class, 'store']);
+// AUTH ROUTES
+Route::prefix('auth')->group(function () {
+    // ACOUNT CONFIRMATION
+    Route::get('email-confirm/{email}', [emailConfirmation::class, 'confirmingEmail']);
+    Route::post('resend-confirmation', [AuthController::class, 'manualSendEmailValidation']);
 
+    // PASSWORD RESET 
+    Route::post('forgot-password', [AuthController::class, 'sendResetPassToken']);
+    Route::get('verify-reset-token/{email}', [emailConfirmation::class, 'verifyResetToken']);
+    Route::post('reset-password', [UserController::class, 'updatePassword']);
 });
 
+
+// PROTECTED ROUTS
+Route::middleware([JwtAuthMiddleware::class])->group(function () {
+
+    // AUTH
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    // USER CRUD
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index']);      // GET /users (Liste tout)
+        Route::put('{user}', [UserController::class, 'update']); // PUT /users/{id}
+        Route::delete('{user}', [UserController::class, 'destroy']); // DELETE /users/{id}
+    });
+});
